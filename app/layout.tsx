68e327569a02dc9877e74a775815527e8664e937
EYITAY <alimieyitayo@gmail.com>
2024-09-30 13:01:50 -0700

    mw

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import './globals.css'
 import PageHeader from '@/components/page-header';
 import Head from 'next/head';
 import Script from 'next/script';
+import myWidget from '@/components/my-widget';
+
 export default function RootLayout({
   children,
 }: {
@@ -16,10 +18,12 @@ export default function RootLayout({
       <html lang="en">
           <Head>
             <Script src="https://rateaway.vercel.app//widget.umd.js"></Script>
+            
           </Head>
           <body>
           <Script src="https://rateaway.vercel.app//widget.umd.js"></Script>
             <PageHeader/>
+            <my-widget></my-widget>
           {children}
         </body>
       </html>
