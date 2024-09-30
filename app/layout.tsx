77c44109365280a4e16c551e02b6e231f76477b7
EYITAY <alimieyitayo@gmail.com>
2024-09-30 08:05:15 -0700

    final

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import './globals.css'
+import PageHeader from '@/components/page-header';
+import Head from 'next/head';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <PageHeader />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
---
+++ lib/payments.ts
@@ -0,0 +1,4 @@
+export const monthlyPlanId: string = "price_1Q3KpmGvBtnC6CHUNyrIUgFa";
+export const yearlyPlanId: string = "price_1Q4JDhGvBtnC6CHUUmpxbXPY";
+export const maxFreeProjects: number = 1;
+
