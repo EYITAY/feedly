443c0ece4cf9c18125a78d04b3c2932f00057842
EYITAY <alimieyitayo@gmail.com>
2024-10-21 07:15:40 -0700

    rateway

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,27 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import Pageheader from "@/components/page-header";
+import Script from "next/script";
 
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
-import './globals.css'
-import PageHeader from '@/components/page-header';
-import Head from 'next/head';
-import Script from 'next/script';
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <Head>
-          <Script src="https://rateaway.vercel.app/widget.umd.js"></Script>
-        </Head>
+        <head>
+          <title>feedly</title>
+        </head>
         <body>
-        <Script src="https://rateaway.vercel.app/widget.umd.js"></Script>
-          <PageHeader />
-          <my-widget></my-widget>
+          <Script src="https://rateaway.vercel.app/widget.umd.js" />
+          <Pageheader />
+          <my-widget project-id="62"></my-widget>
           {children}
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
+
